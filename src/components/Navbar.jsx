b32440b35194dc6ef5ca230cb7171fd01b9e040f
Ammar Ahmed <ammar7298@gmail.com>
2024-09-01 20:33:59 +0500

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
-import { FaBars } from 'react-icons/fa'; // Importing the hamburger icon
+import { FaBars, FaTimes } from 'react-icons/fa'; // Importing the hamburger and close icons
+import { motion } from 'framer-motion'; // Importing framer-motion for animations
 
 const Navbar = () => {
   const router = useRouter();
@@ -15,8 +16,8 @@ const Navbar = () => {
 
   return (
     <div className='w-full flex-col flex'>
-      <header className='w-full h-[48px] flex justify-center items-center lg:px-8 bg-black'>
-        <div className='w-full lg:w-[70vw] xl:w-1/2 flex justify-between items-center text-[#FAFAFA] text-[12px] md:text-[14px] px-4 md:px-0'>
+      <header className='w-full h-[48px] flex justify-center items-center bg-black px-4 md:px-8'>
+        <div className='w-full max-w-screen-lg flex justify-between items-center text-[#FAFAFA] text-[10px] md:text-[14px]'>
           <div className='flex gap-2 md:gap-4'>
             <h3 className='text-[#FAFAFA]'>
               Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
@@ -36,16 +37,18 @@ const Navbar = () => {
         </div>
       </header>
 
-      <nav className='w-full flex items-center justify-between py-4 px-4 md:px-0'>
+      <nav className='w-full flex items-center justify-between py-4 px-4 md:px-8'>
         {/* Hamburger and Search (for mobile view) */}
-        <div className='md:hidden flex justify-between w-full'>
-          
-          <div className='flex justify-center flex-grow'>
-            <div className='flex gap-4 px-4 bg-[#F5F5F5] items-center w-full max-w-xs'>
+        <div className='md:hidden flex items-center justify-between w-full'>
+          <button onClick={toggleMenu} className='focus:outline-none'>
+            <FaBars className='text-black w-6 h-6' />
+          </button>
+          <div className='flex-grow flex justify-center'>
+            <div className='flex gap-2 px-2 bg-[#F5F5F5] items-center w-full max-w-xs'>
               <input
                 type="text"
-                className='w-full py-1 bg-[#F5F5F5] focus:outline-none focus:ring-2 focus:ring-gray-400'
-                placeholder='What are you looking for?'
+                className='w-full py-1 bg-[#F5F5F5] focus:outline-none focus:ring-2 focus:ring-gray-400 placeholder:text-xs'
+                placeholder='Search'
               />
               <img
                 src="/navbar/icon_search.png"
@@ -54,19 +57,33 @@ const Navbar = () => {
               />
             </div>
           </div>
-          <button onClick={toggleMenu} className='focus:outline-none'>
-            <FaBars className='text-black w-6 h-6' />
-          </button>
+          <div className='flex gap-4'>
+            <img
+              src="/navbar/icon_heart.png"
+              className='w-5 cursor-pointer'
+              alt="Wishlist Icon"
+            />
+            <img
+              src="/navbar/icon_cart.png"
+              className='w-6 cursor-pointer'
+              alt="Cart Icon"
+            />
+            <img
+              src="/navbar/icon_user.png"
+              className='w-6 cursor-pointer'
+              alt="User Icon"
+            />
+          </div>
         </div>
 
         {/* Logo, Links, Search, and Icons (for larger screens) */}
-        <div className='hidden md:flex justify-evenly items-center w-full'>
-          <div className='flex items-center'>
-            <h1 className='text-[20px] md:text-[24px] font-bold text-black'>Exclusive</h1>
-          </div>
+        <div className='hidden md:flex items-center justify-evenly w-full'>
+          <h1 className='text-[20px] md:text-[24px] font-bold text-black'>
+            Exclusive
+          </h1>
 
-          <div className='flex items-center gap-16'>
-            <ul className='flex flex-row gap-4 md:gap-16'>
+          <div className='flex items-center gap-8'>
+            <ul className='flex flex-row gap-8'>
               <li className={`text-[14px] md:text-[16px] pb-1 ${isActive('/') ? 'border-b-2 border-black' : ''}`}>
                 <Link href="/" onClick={toggleMenu}>Home</Link>
               </li>
@@ -81,7 +98,7 @@ const Navbar = () => {
               </li>
             </ul>
 
-            <div className='flex justify-center flex-grow px-4'>
+            <div className='flex-grow flex justify-center px-4'>
               <div className='flex gap-4 px-4 bg-[#F5F5F5] items-center w-full max-w-md'>
                 <input
                   type="text"
@@ -116,25 +133,42 @@ const Navbar = () => {
           </div>
         </div>
 
-        {/* Links for mobile view */}
-        <div className={`${
-          isOpen ? 'block' : 'hidden'
-        } md:hidden`}>
-          <ul className='flex flex-col gap-4 mt-4'>
-            <li className={`text-[14px] md:text-[16px] pb-1 ${isActive('/') ? 'border-b-2 border-black' : ''}`}>
-              <Link href="/" onClick={toggleMenu}>Home</Link>
-            </li>
-            <li className={`text-[14px] md:text-[16px] pb-1 ${isActive('/contact') ? 'border-b-2 border-black' : ''}`}>
-              <Link href="/contact" onClick={toggleMenu}>Contact</Link>
-            </li>
-            <li className={`text-[14px] md:text-[16px] pb-1 ${isActive('/about') ? 'border-b-2 border-black' : ''}`}>
-              <Link href="/about" onClick={toggleMenu}>About</Link>
-            </li>
-            <li className={`text-[14px] md:text-[16px] pb-1 ${isActive('/signup') ? 'border-b-2 border-black' : ''}`}>
-              <Link href="/signup" onClick={toggleMenu}>Sign Up</Link>
-            </li>
-          </ul>
-        </div>
+        {/* Mobile Menu Pop-Up */}
+        <motion.div
+          className={`fixed top-0 right-0 w-full h-full bg-white shadow-lg z-50 ${
+            isOpen ? 'translate-x-0' : 'translate-x-full'
+          } transition-transform duration-300 ease-in-out`}
+          initial={{ translateX: '100%' }}
+          animate={{ translateX: isOpen ? '0%' : '100%' }}
+          exit={{ translateX: '100%' }}
+        >
+          <div className='flex flex-col h-full p-4'>
+            <button
+              onClick={toggleMenu}
+              className='self-end text-2xl text-black'
+            >
+              <FaTimes />
+            </button>
+            <div className='flex-grow flex flex-col items-center justify-center'>
+            
+              <ul className='flex flex-col font-serif text-center gap-8'>
+              <li className={`text-[20px] md:text-[16px] pb-1 ${isActive('/') ? 'border-b-2 border-black' : ''}`}>
+                <Link href="/" onClick={toggleMenu}>Home</Link>
+              </li>
+              <li className={`text-[20px] md:text-[16px] pb-1 ${isActive('/contact') ? 'border-b-2 border-black' : ''}`}>
+                <Link href="/contact" onClick={toggleMenu}>Contact</Link>
+              </li>
+              <li className={`text-[20px] md:text-[16px] pb-1 ${isActive('/about') ? 'border-b-2 border-black' : ''}`}>
+                <Link href="/about" onClick={toggleMenu}>About</Link>
+              </li>
+              <li className={`text-[20px] md:text-[16px] pb-1 ${isActive('/signup') ? 'border-b-2 border-black' : ''}`}>
+                <Link href="/signup" onClick={toggleMenu}>Sign Up</Link>
+              </li>
+            </ul>
+          
+            </div>
+          </div>
+        </motion.div>
       </nav>
     </div>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import Navbar from '@/components/Navbar';
 import '../globals.css';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
    return (
     
     <>
+    <Head>
+        <title>E-commerce</title> {/* Set your desired title here */}
+        <meta name="description" content="Exclusive" />
+        {/* You can add more meta tags or other head elements here */}
+      </Head>
     <Navbar/>
       <Component {...pageProps} />
     </>
