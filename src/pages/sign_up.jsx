5b4d322f21031248cb0865b96f18e7bfe5e3791b
Ammar Ahmed <ammar7298@gmail.com>
2024-10-19 23:43:36 +0500

    D

--- src/pages/sign_up.jsx
+++ src/pages/sign_up.jsx
@@ -16,22 +16,32 @@ const SignUp = () => {
         toast.success(`Welcome, ${session.user.name}!`, { position: 'top-center' }); // Show toast message
         toastShown.current = true; // Set ref to true to indicate the toast has been shown
       }
-       // Redirect to the home page if signed in
+      // Redirect to the home page if signed in
     } else {
       toastShown.current = false; // Reset ref if user is not signed in
     }
   }, [session, router]);
+
+
+  const Register = () => {
+    try {
+      signIn('github', { callbackUrl: '/' });
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <div className="w-full md:my-20 py-8 flex">
       <div className="main flex flex-col justify-between md:flex-row w-full">
         {/* Left Image Section */}
         <div className="left w-full h-full md:px-0 px-6 mb-6 md:mb-0">
-          <Image 
-            src="/login/sign-in.png" 
-            alt="Sign In Illustration" 
-            width={600} 
-            height={300} 
+          <Image
+            src="/login/sign-in.png"
+            alt="Sign In Illustration"
+            width={600}
+            height={300}
             className="rounded-lg object-cover"
           />
         </div>
@@ -41,7 +51,7 @@ const SignUp = () => {
           {session ? ( // Check if the user is signed in
             <>
               <h1 className="text-3xl font-semibold mb-4">Welcome, {session.user.name}!</h1>
-              <button 
+              <button
                 className="bg-red-600 text-white py-3 rounded-lg transition-all mb-4"
                 onClick={() => signOut()} // Trigger sign-out
               >
@@ -54,9 +64,9 @@ const SignUp = () => {
               <p className="text-gray-600 mb-6">Use your GitHub account to sign in</p>
 
               {/* GitHub Sign-In Button */}
-              <button 
+              <button
                 className="border border-gray-300 font-semibold py-3 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-100 transition-all"
-                onClick={() => signIn('github', { callbackUrl: '/' })} // Redirect to home after sign-in
+                onClick={() => Register()} // Redirect to home after sign-in
               >
                 <img src="/login/github-icon.png" alt="GitHub Icon" className="w-5 h-5" />
                 Sign in with GitHub
